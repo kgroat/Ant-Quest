19f75c9a5c12f406543ce2c39506e94d9be94cb6
RJ Szura <Rdragon84@gmail.com>
2012-04-16 23:18:05 -0400

    some more character stuff

--- src/antquest/Character.java
+++ src/antquest/Character.java
@@ -5,8 +5,8 @@ import java.util.*;
 
 abstract public class Character 
 {
-    protected Weapon weapon, offhand;
-    protected Armor midbody, lowbody;
-    protected Accesory acc1, acc2, acc3;
+//    protected Weapon weapon, offhand;
+//    protected Armor midbody, lowbody;
+//    protected Accesory acc1, acc2, acc3;
     
 }
