0e6db71d7e1aa151657a4f7b3595c6bd78e21c5e
RJ Szura <Rdragon84@gmail.com>
2012-04-16 22:21:16 -0400

    blah

---
+++ src/antquest/Character.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package antquest;
+
+/**
+ *
+ * @author rdragon84
+ */
+public class Character {
+    
+}
