bea0b57ae8e0745f34837dc81f4b6989faf7de0e
RJ Szura <Rdragon84@gmail.com>
2012-04-17 20:25:36 -0400

    some more character stuff

--- src/antquest/Entity.java
+++ src/antquest/Entity.java
@@ -9,8 +9,53 @@ public class Entity
     protected String character_name;
     protected ArrayList<String> skills;
     
+    public String getName()
+    {
+        return character_name;
+    }
+    
+    public int getHP()
+    {
+        return hp;
+    }
+    
+    public int getAP()
+    {
+        return ap;
+    }
+    
+    public int getDef()
+    {
+        return def;
+    }
+    
+    public int getMdef()
+    {
+        return mdef;
+    }
+    
+    public int getReact()
+    {
+        return react;
+    }
+    
     public int getAtk()
     {
         return atk;
+    }
+    
+    public int getMagic()
+    {
+        return mage;
+    }
+    
+    public int getAcces()
+    {
+        return acc;
+    }
+    
+    public ArrayList<String> getSkills()
+    {
+        return skills;
     }
 }
