81095fd65f9f7f468418f2315a02302f9dc60267
RJ Szura <Rdragon84@gmail.com>
2012-04-17 20:25:48 -0400

    some more character stuff

--- src/antquest/Character.java
+++ src/antquest/Character.java
@@ -11,4 +11,38 @@ abstract public class Character extends Entity
     protected LowerArmor larm;
     protected Accessory acc1, acc2, acc3;
     
+    public Weapon getMainWep()
+    {
+        return weapon;
+    }
+    
+    public Weapon getSecondaryWep()
+    {
+        return offhand;
+    }
+    
+    public UpperArmor getUarmor()
+    {
+        return uarm;
+    }
+    
+    public LowerArmor getLamror()
+    {
+        return larm;
+    }
+    
+    public Accessory getAcc1()
+    {
+        return acc1;
+    }
+    
+    public Accessory getAcc2()
+    {
+        return acc2;
+    }
+    
+    public Accessory getAcc3()
+    {
+        return acc3;
+    }
 }
