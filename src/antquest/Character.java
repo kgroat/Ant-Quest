1e8744f9c11ec718813bdb19b25a88993d41299b
RJ Szura <Rdragon84@gmail.com>
2012-04-17 20:16:49 -0400

    some more character stuff

--- src/antquest/Character.java
+++ src/antquest/Character.java
@@ -4,7 +4,7 @@ import java.util.*;
 import antquest.items.*;
 
 
-abstract public class Character 
+abstract public class Character extends Entity
 {
     protected Weapon weapon, offhand;
     protected UpperArmor uarm;
