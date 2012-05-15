4950f9d5cfec3be9f208c075e368e1c5c9a2b552
Kevin <Kevin@Kevin-PC>
2012-05-15 00:28:48 -0400

    Made sexy cursor for BattleMode.

--- src/antquest/BattleMode.java
+++ src/antquest/BattleMode.java
@@ -6,7 +6,9 @@ package antquest;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Shape;
 import java.awt.event.KeyEvent;
+import java.awt.geom.Line2D;
 import java.util.ArrayList;
 
 /**
@@ -18,11 +20,13 @@ public class BattleMode extends GameMode {
    //Even y-value means it is shifted to the right
    public static final int DEFAULT_WIDTH = 20;
    public static final int DEFAULT_HEIGHT = 15;
+   public static final double PERIOD = 30;
    protected int mapWidth, mapHeight;
    protected int cx, cy;
    protected ArrayList<BattleActor> actors;
    protected BattleActor current;
    protected Hex[][] battlemap;
+   protected double frame;
 
    public BattleMode() {
       battlemap = new Hex[DEFAULT_WIDTH][DEFAULT_HEIGHT];
@@ -35,6 +39,7 @@ public class BattleMode extends GameMode {
       mapHeight = DEFAULT_HEIGHT;
       cx = 0;
       cy = 0;
+      frame = 0;
    }
 
    @Override
@@ -85,7 +90,7 @@ public class BattleMode extends GameMode {
             ERROR.tryPlay(true, false);
          }
       }
-      throw new UnsupportedOperationException("Not supported yet.");
+      //throw new UnsupportedOperationException("Not supported yet.");
    }
 
    public BattleActor actorAtCursor() {
@@ -99,22 +104,51 @@ public class BattleMode extends GameMode {
 
    @Override
    public void render(Graphics2D g) {
-      final int TILE_WIDTH = 20;
+      g.setColor(new Color(128, 128, 128));
+      g.fillRect(0, 0, AQEngine.getWidth(), AQEngine.getHeight());
+      final int SIZE = 20;
       Hex h;
       int terr, rd, gr, bl, tx, ty;
       Color c;
+      Shape s;
+      float cos = (float)(Math.cos(frame / PERIOD * Math.PI * 2)/2+.5);
+      float bty;
       for (int y = 0; y < mapHeight; y++) {
          for (int x = 0; x < mapWidth; x++) {
             h = battlemap[x][y];
             terr = h.getTerrain();
-            tx = x * TILE_WIDTH + 75;
-            ty = y * TILE_WIDTH * 3 / 4 + 50;
+            tx = x * SIZE + 75;
+            ty = y * SIZE * 3 / 4 + 50;
             if (y % 2 == 0) {
-               tx += TILE_WIDTH / 2;
+               tx += SIZE / 2;
             }
-            drawHex(tx, ty, TILE_WIDTH, terr, h.getZ(), g);
+            drawHex(tx, ty, SIZE, terr, h.getZ(), g);
+            if (cx == x && cy == y) {
+               System.out.println(cos + " / " + (int) (255 * cos));
+               g.setColor(new Color((int) (180 + 75 * cos), (int) (180 + 75 * cos), (int) (255 * cos)));
+               bty = ty - h.getZ() - cos * 7.5f-.2f;
+               g.draw(new Line2D.Float(tx, bty + SIZE / 4, tx + SIZE / 2, bty));
+               g.draw(new Line2D.Float(tx + SIZE, bty + SIZE / 4, tx + SIZE / 2, bty));
+            }
+            //Render the BattleActor on the hex, if one exists
+         }
+         if(cy == y){
+            h = battlemap[cx][cy];
+            tx = cx * SIZE + 75;
+            ty = cy * SIZE * 3 / 4 + 50;
+            if (y % 2 == 0) {
+               tx += SIZE / 2;
+            }
+            g.setColor(new Color((int) (180 + 75 * cos), (int) (180 + 75 * cos), (int) (255 * cos)));
+            bty = ty - h.getZ() - cos * 7.5f-.2f;
+            g.draw(new Line2D.Float(tx, bty + SIZE * 3/ 4, tx + SIZE / 2, bty + SIZE));
+            g.draw(new Line2D.Float(tx + SIZE, bty + SIZE * 3 / 4, tx + SIZE / 2, bty + SIZE));
+            g.draw(new Line2D.Float(tx, bty + SIZE / 4, tx, bty + SIZE * 3 / 4));
+            g.draw(new Line2D.Float(tx + SIZE, bty + SIZE / 4, tx + SIZE, bty + SIZE * 3 / 4));
          }
       }
+
+      frame = (frame + 1) % PERIOD;
       //throw new UnsupportedOperationException("Not supported yet.");
    }
 
@@ -178,7 +212,7 @@ public class BattleMode extends GameMode {
          g.fillRect(px + size / 2 - i * 2, py + size - i, 4 * i, 1);
       }
       g.setColor(c);
-      g.drawLine(px, py + size *3 / 4, px + size / 2, py + size);
+      g.drawLine(px, py + size * 3 / 4, px + size / 2, py + size);
       g.drawLine(px + size, py + size * 3 / 4, px + size / 2, py + size);
       g.setColor(Color.BLACK);
       g.fillRect(px, py + size / 4, 1, size * 5 / 2);
